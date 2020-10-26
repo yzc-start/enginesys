7c7fc14d70a41254d94705db47520fa591c43927
Administrator <821454058@qq.com>
2020-10-26 21:59:30 +0800

    version2.0 解决跨域问题

---
+++ src/main/java/com/example/demo/config/loginConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.config;
+
+/**
+ * @author jiangfeng
+ * @date 2020/10/26
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+/**
+ * 解决跨域问题
+ */
+@Configuration
+public class loginConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); //允许任何域名
+        corsConfiguration.addAllowedHeader("*"); //允许任何头
+        corsConfiguration.addAllowedMethod("*"); //允许任何方法
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); //注册
+        return new CorsFilter(source);
+    }
+}
